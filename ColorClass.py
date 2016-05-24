ede01ef9e00be58739a79cdebcf60727b04ffbce
Michael-Chavez <michael.chavez@ms223.org>
2016-05-24 09:29:13 -0400

    Create ColorClass.py

---
+++ ColorClass.py
@@ -0,0 +1,19 @@
+class Color(object):
+
+Red = Color()
+Blue =  Color()
+Green = Color()
+
+Red.r = 255
+Red.g = 51
+Red.b = 51
+
+Blue.r = 51
+Blue.g = 51
+Blue.b = 255
+
+Green.r = 51
+Green.g = 255
+Green.b = 51
+
+def
